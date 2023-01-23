5bbaff7f9007000cacfff0f5eb0afd98aab11348
zxamu <jsszmdpnc@outlook.com>
2023-01-23 15:06:10 -0600

    cambio de prueba

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@ import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <AppLayout title="Zxamu">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
