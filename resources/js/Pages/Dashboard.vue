3ea4f9ee36500e7f9b15cf24df7c040b18a571d0
zxamu <jsszmdpnc@outlook.com>
2023-01-23 15:08:36 -0600

    eliminacion de cambios

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@ import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Zxamu">
+    <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
