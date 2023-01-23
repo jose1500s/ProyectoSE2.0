84b03ee99732d94912b87432bc1f515389ad75b5
Omaid1727 <122405314+Omaid1727@users.noreply.github.com>
2023-01-23 16:16:00 -0600

    cancelar

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use resources\js\Pages\Auth\Login;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Login', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
