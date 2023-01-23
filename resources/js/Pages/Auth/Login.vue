8e4c5dec80a80c93cd00485753545a41f91672af
jose antonio patiño <77465536+jose1500s@users.noreply.github.com>
2023-01-23 14:02:55 -0800

    redireccion al login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -74,7 +74,7 @@ const submit = () => {
                     <span class="ml-2 text-sm text-gray-600">Remember me</span>
                 </label>
             </div>
-
+         
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
                     Forgot your password?
