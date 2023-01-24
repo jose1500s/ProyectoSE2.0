26513f89655bf33add6dd3fe61cd211fcf08226a
zxamu <jsszmdpnc@outlook.com>
2023-01-23 23:02:06 -0600

    Redireccion a dashboard y eliminacion de comentarios

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -31,191 +31,94 @@ const submit = () => {
 
 
 <template>
-<section class="bg-white">
-    <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
-      <aside
-        class="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6"
-      >
-        <img
-          alt="Pattern"
-          src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          class="absolute inset-0 h-full w-full object-cover"
-        />
-      </aside>
-  
-      <main
-        aria-label="Main"
-        class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
-      >
-        <div class="max-w-xl lg:max-w-3xl">
-          <a class="block text-blue-600" href="/">
-            <span class="sr-only">Home</span>
-            
-          </a>
-  
-          <h1
-            class="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
-          >
-            Servicios Escolares 📖
-          </h1>
-  
-          <p class="mt-4 leading-relaxed text-gray-500">
-            por si acaso
-          </p>
-  
-          <form action="#" class="mt-8 grid grid-cols-6 gap-6">
-  
-            <div class="col-span-6">
-              <label for="Email" class="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-  
-              <input
-                type="email"
-                id="Email"
-                name="email"
-                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-              />
-            </div>
-  
-            <div class="col-span-6 sm:col-span-3">
-              <label
-                for="Password"
-                class="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-  
-              <input
-                type="password"
-                id="Password"
-                name="password"
-                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-              />
-            </div>
-  
-            <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
-              <PrimaryButton 
-                class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Acceder
-              </PrimaryButton> 
-  
-              <p class="mt-4 text-sm text-gray-500 sm:mt-0">
-                    <Link v-if="canResetPassword" :href="route('register')" class="text-gray-700 underline">
-                        Registrarse
-                    </Link>
-              </p>
-              <p class="mt-4 text-sm text-gray-500 sm:mt-0">
-                    <Link v-if="canResetPassword" :href="route('password.request')" class="text-gray-700 underline">
-                        ¿Olvidaste tu contraseña?
-                    </Link>
-              </p>
-            </div>
-          </form>
-        </div>
-      </main>
-    </div>
-  </section>
-</template>
-
-<!-- 
-<script setup>
-import { Head, Link, useForm } from '@inertiajs/vue3';
-import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import Checkbox from '@/Components/Checkbox.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-
-defineProps({
-    canResetPassword: Boolean,
-    status: String,
-});
-
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
-});
-
-const submit = () => {
-    form.transform(data => ({
-        ...data,
-        remember: form.remember ? 'on' : '',
-    })).post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
-</script>
-
-<template>
     <Head title="Log in" />
-
-    <AuthenticationCard>
-        <template #logo>
-            <AuthenticationCardLogo />
-        </template>
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-6">
-                <label class="flex items-center">
-                    <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-        
-            <div class="h-20 grid grid-cols-2 gap-3 content-start">
-                <div>
-                    <Link v-if="canResetPassword" :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                        Registrarse
-                    </Link>
+    <section class="bg-white">
+        <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+          <aside
+            class="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6"
+          >
+            <img
+              alt="Pattern"
+              src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              class="absolute inset-0 h-full w-full object-cover"
+            />
+          </aside>
+      
+          <main
+            aria-label="Main"
+            class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
+          >
+            <div class="max-w-xl lg:max-w-3xl">
+              <a class="block text-blue-600" href="/">
+                <span class="sr-only">Home</span>
+                
+              </a>
+      
+              <h1
+                class="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
+              >
+                Servicios Escolares 📖
+              </h1>
+      
+              <p class="mt-4 leading-relaxed text-gray-500">
+                por si acaso
+              </p>
+              <form @submit.prevent="submit" class="mt-8 grid grid-cols-6 gap-6">
+                
+                <div class="col-span-6">
+                  <div>
+                    <InputLabel for="email" value="Email" />
+                    <TextInput
+                        id="email"
+                        v-model="form.email"
+                        type="email"
+                        class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                        required
+                        autofocus
+                    />
+                    <InputError class="mt-2" :message="form.errors.email" />
                 </div>
-            
-                <div class="flex item-center">
-                    <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                        ¿Olvidaste tu contraseña?
-                    </Link>
+                 
                 </div>
-            </div>
-
-            <div>
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+      
+                <div class="col-span-6">
+                  <InputLabel for="password" value="Password" />
+                    <TextInput
+                        id="password"
+                        v-model="form.password"
+                        type="password"
+                        class="mt-1 block w-full"
+                        required
+                        autocomplete="current-password"
+                    />
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+      
+                <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
+                  <PrimaryButton 
+                    class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Acceder
-                </PrimaryButton>
-            </div>
-
-        </form>
-    </AuthenticationCard>
-</template> -->
+                  </PrimaryButton> 
+      
+                  <p class="mt-4 text-sm text-gray-500 sm:mt-0">
+                        <Link v-if="canResetPassword" :href="route('register')" class="text-gray-700 underline">
+                            Registrarse
+                        </Link>
+                  </p>
+                  <p class="mt-4 text-sm text-gray-500 sm:mt-0">
+                        <Link v-if="canResetPassword" :href="route('password.request')" class="text-gray-700 underline">
+                            ¿Olvidaste tu contraseña?
+                        </Link>
+                  </p>
+                </div>
+              </form>
+          </div>
+        </main>
+      </div>
+    </section>
+</template>
