474bd5f4fe9ae95fa914b5ffbb4fc3d2abb075c7
jose antonio patiño <77465536+jose1500s@users.noreply.github.com>
2023-01-25 09:19:21 -0800

    no hacer caso a estos cambios

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -15,11 +15,7 @@ defineProps({
 
 <template>
     <AppLayout title="Profile">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Profile
-            </h2>
-        </template>
+        
 
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
