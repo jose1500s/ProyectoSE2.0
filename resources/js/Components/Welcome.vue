f7e38f5c5871dd8572846f7442a35cc8c24b7d91
jose antonio patiño <77465536+jose1500s@users.noreply.github.com>
2023-01-30 20:59:27 -0800

    pruebaXD

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -7,7 +7,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
         <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
             <div class="mx-auto max-w-3xl text-center">
                 <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">
-                    PRUEBA Bienvenido a el sistema de servicios Estudiantiles de la Universidad Politécnica de Querétaro
+                    PRUEBA XD Bienvenido a el sistema de servicios Estudiantiles de la Universidad Politécnica de Querétaro
                 </h2>
 
                 <p class="mt-4 text-gray-500 sm:text-xl">
