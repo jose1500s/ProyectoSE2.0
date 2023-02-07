5719eedd2145fd43ce8bf9199fda743c645ce747
jose1500s <120035990@upq.edu.mx>
2023-02-06 22:46:34 -0600

    mas filtros y boton resetear en ingreso.vue

--- resources/js/Pages/menusComponentes/Ingreso.vue
+++ resources/js/Pages/menusComponentes/Ingreso.vue
@@ -6,6 +6,7 @@ import InputText from 'primevue/inputtext';
 import Row from 'primevue/row';
 import Button from 'primevue/button';
 import { FilterMatchMode, FilterService } from 'primevue/api';
+import MultiSelect from 'primevue/multiselect';
 
 export default {
   components: {
@@ -15,6 +16,7 @@ export default {
     InputText,
     Row,
     Button,
+    MultiSelect,
   },
   props: {
     ingresos: Array,
@@ -37,16 +39,34 @@ export default {
       const procesosFiltradas = [...new Set(procesos)];
       return procesosFiltradas;
     },
+    filtrarFecha() {
+      // filtar del arreglo ingresos.carrera, todos los que se repiten
+      // y retornar un arreglo con los nombres de las carreras sin repetir
+      const fechas = this.ingresos.map(item => item.fecha);
+      const fechasFiltradas = [...new Set(fechas)];
+      return fechasFiltradas;
+    },
     exportCSV() {
       this.$refs.dt.exportCSV();
+    },
+    limpiarFiltros() {
+      // limpia/eliminar los filtros realizados en la  tabla y volver a mostrar todos los datos
+      this.filters.carrera.value = null;
+      this.filters.Proceso.value = null;
+      this.filters.fecha.value = null;
+      this.$refs.dt.filter(this.filters, 'carrera');
+      this.$refs.dt.filter(this.filters, 'Proceso');
+      this.$refs.dt.filter(this.filters, 'fecha');
     },
   },
   data() {
     return {
       filters: {
-        'carrera': { value: null, matchMode: FilterMatchMode.CONTAINS },
-        'Proceso': { value: null, matchMode: FilterMatchMode.EQUALS },
+        'carrera': { value: null, matchMode: FilterMatchMode.IN },
+        'Proceso': { value: null, matchMode: FilterMatchMode.IN },
+        'fecha': { value: null, matchMode: FilterMatchMode.IN },
       },
+      noDataMessage: 'No se encontraron datos'
     };
   },
 
@@ -61,23 +81,30 @@ export default {
     </template>
     <section class="bg-white">
 
-      <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8 p-[20px]">
+      <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 p-[20px]">
+        <div class="text-center mb-5">
+          <div class="flex gap-5 justify-center">
+            <Button icon="pi pi-external-link" label="Exportar Excel" @click="exportCSV($event)" />
 
 
-        <DataTable :value="ingresos" :paginator="true" class="p-datatable-customers" :rows="10" ref="dt" 
-          v-model:filters="filters">
-          <div class="text-center mb-5">
-            <div class="flex gap-5 justify-center">
-              <Button icon="pi pi-external-link" label="Export Excel" @click="exportCSV($event)" />
-              <select v-model="filters.carrera.value">
-                <option class="" v-for="carrera in filtrarCarreras()" :key="carrera" :value="carrera">{{ carrera }}</option>
-              </select>
-              <select v-model="filters.Proceso.value">
-                <option class="" v-for="procesos in filtrarProcesos()" :key="procesos" :value="procesos">{{ procesos }}
-                </option>
-              </select>
-            </div>
+            <MultiSelect v-model="filters.carrera.value" :options="filtrarCarreras()" placeholder="Carrera"
+              display="chip" />
+
+            <MultiSelect v-model="filters.Proceso.value" :options="filtrarProcesos()" placeholder="Proceso"
+              display="chip" />
+
+            <MultiSelect v-model="filters.fecha.value" :options="filtrarFecha()" placeholder="Fecha"
+              display="chip" />
+
+            <Button icon="pi pi-times" label="Limpiar" @click="limpiarFiltros()" />
+
+
           </div>
+        </div>
+
+        <DataTable :value="ingresos" :paginator="true" class="p-datatable-customers" :rows="10" ref="dt"
+          v-model:filters="filters" :emptyMessage="noDataMessage">
+
           <Column field="carrera" header="Carrera" :sortable="true"></Column>
           <Column field="aspirantes" header="Aspirantes" :sortable="true"></Column>
           <Column field="examinados" header="Examinados" :sortable="true"></Column>
@@ -87,6 +114,14 @@ export default {
           <Column field="Proceso" header="Proceso" :sortable="true"></Column>
           <Column field="fecha" header="Fecha" :sortable="true"></Column>
 
+          <!-- mensaje de no hay datos -->
+          <template #empty>
+              <div class="flex justify-center align-middle text-xl">
+                <h2>No se encontraron datos</h2>
+              </div>
+          </template>
+
+
         </DataTable>
 
       </div>
@@ -94,3 +129,8 @@ export default {
   </AppLayout>
 </template>
 
+<style type="text/css">
+.p-multiselect-label-container {
+  max-width: 170px;
+}
+</style>
