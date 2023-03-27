40dc66c190d387af97c8c28d3864e0d04ff64670
Omaid1727 <122405314+Omaid1727@users.noreply.github.com>
2023-03-26 20:57:55 -0600

    componentes para transporte

---
+++ resources/js/Pages/menusComponentes/Transporte/TabMenu.vue
@@ -0,0 +1,53 @@
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Ingreso from "@/Pages/menusComponentes/Ingreso/Ingreso.vue";
+import Equivalencia from "@/Pages/menusComponentes/Ingreso/Equivalencia.vue";
+import Maestrias from "@/Pages/menusComponentes/Ingreso/Maestrias.vue";
+import NIngreso from "@/Pages/menusComponentes/Ingreso/NuevoIngreso.vue";
+import Reingreso from "@/Pages/menusComponentes/Ingreso/Reingreso.vue";
+
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+
+export default {
+    components: {
+        TabView,
+        TabPanel,
+        AppLayout,
+        Ingreso,
+        Equivalencia,
+        Maestrias,
+        NIngreso,
+        Reingreso,
+    },
+    props: {
+        ingresos: Array,
+        maestrias: Array,
+        equivalencias: Array,
+        ningresos: Array,
+        reingresos: Array,
+    },
+}
+</script>
+
+<template>
+    <AppLayout title="Ingreso">
+        <TabView ref="tabview1" class="p-3">
+            <TabPanel header="Admision">
+                <Ingreso :ingresos="ingresos" />
+            </TabPanel>
+            <TabPanel header="Nuevo Ingreso">
+                <NIngreso :ningresos="ningresos" />
+            </TabPanel>
+            <TabPanel header="Reingreso">
+                <Reingreso :reingresos="reingresos" />
+            </TabPanel>
+            <TabPanel header="Equivalencia">
+                <Equivalencia :equivalencias="equivalencias" />
+            </TabPanel>
+            <TabPanel header="Maestrias">
+                <Maestrias :maestrias="maestrias" />
+            </TabPanel>
+        </TabView>
+    </AppLayout>
+</template>
