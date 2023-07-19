fd1d4693e864bf734af912709e021ee691ef8de0
Omaid1727 <122405314+Omaid1727@users.noreply.github.com>
2023-07-19 08:51:07 -0600

    porque-el-piche-vscode-lo-pide

--- vendor/composer/ClassLoader.php
+++ vendor/composer/ClassLoader.php
@@ -42,6 +42,9 @@ namespace Composer\Autoload;
  */
 class ClassLoader
 {
+    /** @var \Closure(string):void */
+    private static $includeFile;
+
     /** @var ?string */
     private $vendorDir;
 
@@ -563,12 +566,16 @@ class ClassLoader
             return;
         }
 
-/**
- * Scope isolated include.
- *
- * Prevents access to $this/self from included files.
- */
-function includeFile($file)
-{
-    include $file;
+        /**
+         * Scope isolated include.
+         *
+         * Prevents access to $this/self from included files.
+         *
+         * @param  string $file
+         * @return void
+         */
+        self::$includeFile = static function($file) {
+            include $file;
+        };
+    }
 }
