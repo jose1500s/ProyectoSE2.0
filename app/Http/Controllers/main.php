1e63193f5ef495dbc4f483f9036b942b45359574
JohnMRivera <johnrivera310802@gmail.com>
2023-08-08 22:57:35 -0600

    Se recuperan roles de usuario para las vistas

--- app/Http/Controllers/main.php
+++ app/Http/Controllers/main.php
@@ -12,6 +12,7 @@ use App\Models\tb_nuevo_ingreso;
 use App\Models\tb_re_ingreso;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 class main extends Controller
 {
    public function ingreso()
@@ -28,6 +29,8 @@ class main extends Controller
          }
       }
       
+      $roles = Role::all();
+
       // traer de la tabla tb_admision todos los registros
       $ingresos = tb_admision::all();
       $maestrias = tb_maestria::all();
@@ -35,34 +38,76 @@ class main extends Controller
       $ningresos = tb_nuevo_ingreso::all();
       $reingresos = tb_re_ingreso::all();
       // retornar con Inertia a menusComponentes/TabMenu y pasarle los registros
-      return Inertia::render('menusComponentes/Ingreso/TabMenu', ['usuario ' => $usuario, 'permisos' => $permisos,'maestrias' => $maestrias,'ingresos' => $ingresos, 'equivalencias' => $equivalencias, 'ningresos' => $ningresos, 'reingresos' => $reingresos]);
+      return Inertia::render('menusComponentes/Ingreso/TabMenu', ['usuario ' => $usuario, 'roles' => $roles, 'permisos' => $permisos,'maestrias' => $maestrias,'ingresos' => $ingresos, 'equivalencias' => $equivalencias, 'ningresos' => $ningresos, 'reingresos' => $reingresos]);
    }
 
    public function bajas() {
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/Bajas');
    }
 
    public function matricula(){
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/Matricula');
    }
 
    public function egresados(){
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/Egresados');
    }
 
    public function titulados(){
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/Titulados');
    }
 
    public function becas(){
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/Becas');
    }
 
    public function transporte(){
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/Transporte');
    }
 
    public function cambioDeCarrera(){
+      $roles = Auth::user()->roles;
+
+      foreach($roles as $rol){
+         $rol->permissions;
+      }
+      
       return Inertia::render('menusComponentes/CambioDeCarrera');
    }
 
